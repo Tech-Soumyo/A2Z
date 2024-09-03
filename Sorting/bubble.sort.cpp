2c717cba9a58dc154ab2a64e388ecc485df2a77c
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-09-03 15:15:07 +0530

    bubble

---
+++ Sorting/bubble.sort.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    // Function to sort the array using bubble sort algorithm.
+    void bubbleSort(int arr[], int n)
+    {
+        // Your code here
+        for (int i = 1; i < n; i++)
+        {
+
+            for (int j = 0; j < n - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    swap(arr[j], arr[j + 1]);
+                }
+            }
+        }
+    }
+};
