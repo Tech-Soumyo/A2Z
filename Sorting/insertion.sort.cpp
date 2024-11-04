54aa7f1f2495032c03ba3450d99005130fa0de25
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2024-11-04 23:55:26 +0530

    sort

---
+++ Sorting/insertion.sort.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void insertSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int temp = arr[i];
+        int j = i - 1;
+        for (; j >= 0; j--)
+        {
+            if (arr[j] > temp)
+            {
+                arr[j + 1] = arr[j];
+            }
+            else
+                break;
+        }
+        arr[j + 1] = temp;
+    }
+}
